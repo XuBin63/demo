71380262b27096a605a8f817f0ec663fb0116eaa
xubin <1285075239@qq.com>
2018-06-27 14:04:23 +0800

    first commit

---
+++ JS/js.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+window.onload=function(){
+	var obtn=document.getElementById('btn');
+	var omnav=document.getElementById('mnav');
+	obtn.onclick=function(){
+		if(omnav.style.display=="block"){
+			omnav.style.display="none";
+		}
+		else{
+			omnav.style.display="block"
+		}
+	}
+}
